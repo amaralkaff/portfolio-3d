9f58917f6a1c155c72d7f17dfcaccdde06383d5d
amaralkaff <tpamangly@gmail.com>
2024-07-29 12:36:33 +0800

    feat: add 3d text

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,24 +1,51 @@
 import Link from "next/link";
 
 const Header = () => {
-    return (
-        <div className="flex px-8 py-4 justify-between items-center">
-            <div className="font-bold text-3xl">
-                amangLy.
-            </div>
-            <div className="bg-black flex px-2 py-2 space-x-4">
-                <Link href="/about">
-                    <span className="text-white flex justify-center px-2">About</span>
-                </Link>
-                <Link href="/projects">
-                    <span className="text-white flex justify-center px-2">Projects</span>
-                </Link>
-                <Link href="/contact">
-                    <span className="text-white flex justify-center px-2">Contact</span>
-                </Link>
-            </div>
-        </div>
-    );
-}
+  return (
+    <header>
+      <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <ul className="navigation max-w-[90vw] flex flex-wrap justify-between items-center relative mx-auto py-8">
+          <Link href="#">
+            <span className="logo">
+              <h3 className="font-bold text-2xl">amangLy.</h3>
+            </span>
+          </Link>
+          <input type="checkbox" id="check" className="hidden" />
+
+          <span className="menu">
+            <li>
+              <Link href="#">
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <span>About</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <span>Projects</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <span>Contact</span>
+              </Link>
+            </li>
+
+            <label htmlFor="check" className="close-menu">
+              X
+            </label>
+          </span>
+
+          <label htmlFor="check" className="open-menu">
+            Menu
+          </label>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,13 @@
 "use client";
 import Image from "next/image";
-import Header from "./components/header";
-import Spline from "@splinetool/react-spline";
-import section from "./components/section";
+import Header from "./components/Header";
+import Section from "./components/Section";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <div className="w-[70vw] text-[40px] leading-none text-gray-600 px-8 pt-[50px]">
-        Welcome to my portfolio! and I am a software engineer.
-      </div>
-      <div className="w-[80vw] text-[80] leading-none text-black px-8">
-        <br/>
-        Have a look at my projects and feel free to contact me. <br/>
-      </div>
-      <div className="h-[40vh] w-[80vw] mx-auto">
-        <Spline scene="https://prod.spline.design/C0qbOpPBe09j1M1s/scene.splinecode" />
-      </div>
+      <Header />
+      <Section />
     </div>
   );
 }
